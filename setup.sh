1a31265a42fdef80de23212b954b9fc6a9c4f519
acharlas <acharlas@student.42.fr>
2022-02-27 02:15:36 +0100

    maj

--- setup.sh
+++ setup.sh
@@ -4,17 +4,17 @@ CONTAINERS="influxdb grafana mysql phpmyadmin wordpress nginx ftps"
 
 function build_image {
 	echo "Building image for $1"
-	docker build $1 -t acharlas/$1 # > /dev/null 2>&1
+	docker build srcs/$1 -t acharlas/$1 # > /dev/null 2>&1
 }
 
 function create_deployment {
 	echo "Creating deployment for $1"
-	kubectl apply -f $1/$1.yaml # > /dev/null 2>&1
+	kubectl apply -f srcs/$1/$1.yaml # > /dev/null 2>&1
 }
 
 function delete_deployment {
 	echo "Deleting deployment for $1"
-	kubectl delete -f $1/$1.yaml # > /dev/null 2>&1
+	kubectl delete -f srcs/$1/$1.yaml # > /dev/null 2>&1
 }
 
 function generate_ssl {
@@ -23,7 +23,7 @@ function generate_ssl {
 	openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 1 -nodes -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=acharlas/CN=ft_services/emailAddress=acharlas@student.42.fr" > /dev/null 2>&1
 	for image in nginx ftps phpmyadmin
 	do
-		cp key.pem cert.pem $image/
+		cp key.pem cert.pem srcs/$image/
 	done
 }
 
@@ -33,7 +33,7 @@ function install_metallb {
 	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
 
 	#Setup metalLB
-	kubectl apply -f metallb/metalLB.yaml
+	kubectl apply -f srcs/metallb/metalLB.yaml
 
 }
 
@@ -70,10 +70,10 @@ case $1 in
 		#Building docker images
 		$0 build
 		$0 create
-		wait_containers
-		echo "Opening http://172.17.0.2 in firefox"
-		firefox -private-window http://172.17.0.2 &
-		minikube dashboard # > /dev/null 2>&1
+		#wait_containers
+		#echo "Opening http://172.17.0.2 in firefox"
+		#firefox -private-window http://172.17.0.2 &
+		#minikube dashboard # > /dev/null 2>&1
 	;;
 
 	#Creating deployments
@@ -101,8 +101,8 @@ case $1 in
 	cleanup)
 		echo "Cleaning up"
 		$0 reset
-		rm -f key.pem cert.pem
-		for folder in ftps nginx phpmyadmin
+		rm -f srcs/key.pem srcs/cert.pem
+		for folder in srcs/ftps srcs/nginx srcs/phpmyadmin
 		do
 			rm -f $folder/key.pem $folder/cert.pem
 		done
